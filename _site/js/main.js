cc0832207967b31ea028b4de30b648801d25b0ea
eviip <eviiplatform@gmail.com>
2018-01-23 19:41:19 -0800

    Finish form and add submit target
    Activate the form with basic browser validation.
    The Google Sheet was set up and is receiving POSTs from the form.

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,34 +1,35 @@
 $(function() {
 
-    // $('.collapse').collapse('hide');
-    $('.list-group-item.active').parent().parent('.collapse').collapse('show');
+  // $('.collapse').collapse('hide');
+  $('.list-group-item.active').parent().parent('.collapse').collapse('show');
+  // Markdown plain out to bootstrap style
+  $('#markdown-content-container table').addClass('table');
+  $('#markdown-content-container img').addClass('img-responsive');
 
 
-    var pages = new Bloodhound({
-        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('title'),
-        // datumTokenizer: Bloodhound.tokenizers.whitespace,
-        queryTokenizer: Bloodhound.tokenizers.whitespace,
-
-        prefetch: baseurl + '/search.json'
+  $('#beta-form').on('submit', function(e) {
+    e.preventDefault();
+    $('#beta-form input[type=submit]').attr('disabled', 'disabled');
+    $('#spinner').toggle();
+    var formData = $('#beta-form').serialize();
+    
+    $.ajax({
+      type: 'POST',
+      url: e.target.action,
+      data: formData,
+      success: function(data){
+        
+        $('#beta-form input').val("");
+        $('#beta-form input[type=submit]').prop('disabled', false);
+        $('.response-success').toggle();
+        $('#spinner').toggle();
+      },
+      error: function(xhr, type){
+        $('#beta-form input[type=submit]').prop('disabled', false);
+        $('.response-error').toggle();
+        $('#spinner').toggle();
+        console.error(err);
+      }
     });
-
-    $('#search-box').typeahead({
-        minLength: 0,
-        highlight: true
-    }, {
-        name: 'pages',
-        display: 'title',
-        source: pages
-    });
-
-    $('#search-box').bind('typeahead:select', function(ev, suggestion) {
-        window.location.href = suggestion.url;
-    });
-
-
-    // Markdown plain out to bootstrap style
-    $('#markdown-content-container table').addClass('table');
-    $('#markdown-content-container img').addClass('img-responsive');
-
-
+  });
 });
